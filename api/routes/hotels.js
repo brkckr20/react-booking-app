566679934c779cf6a4e06d456b0e7bf62aad521c
brkckr20 <burak2043@gmail.com>
2022-05-10 15:20:00 +0300

    hotels api kismi yazildi ve basit CRUD islemleri gerceklestirildi

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,71 @@
 import express from 'express'
+import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
+/* hotel ekleme silme güncelleme işlemlerini içeren api */
 
+//create
+router.post("/", async (req, res) => {
+
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+//update
+router.put("/:id", async (req, res) => {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true } // veri ekleme işlemi sonrasında geri döndürülen verinin güncellenmiş veri olması için eklendi.
+        )
+        res.status(200).json(updatedHotel);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//delete
+router.delete("/:id", async (req, res) => {
+
+    try {
+        await Hotel.findByIdAndDelete(
+            req.params.id
+        )
+        res.status(200).json("Hotel silindi...");
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+//get
+router.get("/:id", async (req, res) => {
+
+    try {
+        const hotel = await Hotel.findById(
+            req.params.id
+        )
+        res.status(200).json(hotel);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+//get all
+router.get("/", async (req, res) => {
+
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 export default router;
